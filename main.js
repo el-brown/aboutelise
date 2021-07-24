24e99dd66fc9de691ff1163e3a8e57966ea46889
Elise Brown <elisemarie88@gmail.com>
2021-07-24 16:29:46 -0600

    onclick javascript on truths page.

--- main.js
+++ main.js
@@ -1,1 +1,8 @@
-alert('Welcome to my about me site!')
+// alert('Welcome to my about me site!')
+
+function checkResult() {
+  var value = document.getElementById("truths").elements["truths"].value;
+  if (["bear","garage","ghost","nile","hospital","nazis"].includes(value)) document.getElementById("result").innerHTML = "Correct"
+    else document.getElementById("result").innerHTML = "Incorrect";
+}
+
